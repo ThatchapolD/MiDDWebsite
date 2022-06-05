17d61f07a479afc88512cd8366b4c8155d3fe992
TD Kong <97189852+ThatchapolD@users.noreply.github.com>
2022-06-05 13:30:11 +0700

    Add Files Via Upload
    You can put each file into a different folder so that it won't get messy.

---
+++ Website_Testing/JS_Folder/Navbar.js
@@ -0,0 +1,8 @@
+// let toggleButton = document.getElementsByClassName("toggle-button")[0];
+// let navbarlinks = document.getElementsByClassName("navbar-links")[0];
+
+// toggleButton.addEventListener("click", () => {
+//   navbarlinks.classList.toggle("active");
+// });
+
+window.onscroll = function () {};
